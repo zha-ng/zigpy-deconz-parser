fd0c0bc214d60cac90e7ef00e85766c94a03832d
Alexei Chetroi <alexei.chetroi@outlook.com>
2019-06-14 16:09:39 -0400

    Allow calling from CLI.

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-"""Setup module for zigpy_deconz_parser"""
+"""Setup module for zigpy_deconz_parser. """
 
 from setuptools import find_packages, setup
 
 setup(
     name="zigpy-deconz-parser",
-    version="0.0.1",
-    description="Homeassistant debug log parser for zigpy-deconz radio",
+    version="0.0.2",
+    description="Homeassistant log file parser for zigpy-deconz radio",
     url="http://github.com/zha-ng/zigpy-deconz-parser",
     author="Alexei Chetroi",
     author_email="alexei.chetroi@outlook.com",
     license="GPL-3.0",
     packages=find_packages(exclude=['*.tests']),
+    entry_points={
+        'console_scripts': [
+            'zigpy-deconz-parser=zigpy_deconz_parser.main:main'
+        ],
+    },
     install_requires=[
         'pyserial-asyncio',
         'zigpy-homeassistant',
--- zigpy_deconz_parser/main.py
+++ zigpy_deconz_parser/main.py
@@ -1,11 +1,46 @@
-import binascii
+import getopt
+import os.path
+import sys
 
 import zigpy_deconz_parser.parser as parser
 
 IF_NAME = '/home/ha/.homeassistant/home-assistant.log'
 
+
+def main():
+    argv = sys.argv[1:]
+    infile = None
+    try:
+        opts, args = getopt.getopt(argv, "hi:", ["in-file=",])
+    except getopt.GetoptError:
+        help()
+        sys.exit(2)
+
+    for opt, args in opts:
+        if opt == '-h':
+            help()
+            sys.exit(0)
+        elif opt in ('-i', '--in-file'):
+            infile = args
+
+    if infile is None:
+        help()
+        sys.exit(2)
+
+    proccess(infile)
+
+
 if __name__ == '__main__':
-    with open(IF_NAME, mode='r') as infile:
-        for line in infile:
+    main()
+
+
+def proccess(file):
+    with open(file, mode='r') as file:
+        for line in file:
             print(line.strip())
             parser.parse(line)
+
+
+def help():
+    name = os.path.basename(sys.argv[0])
+    print(name + " -i <input file name>")
